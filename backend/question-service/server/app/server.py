6fb5010176975d4435097ad8793aabfd98cd52b3
Josephaedan <josephaedan@gmail.com>
2023-09-21 00:40:15 +0800

    Add Enum to JSendStatus and HTTPException raises

--- backend/question-service/server/app/server.py
+++ backend/question-service/server/app/server.py
@@ -6,6 +6,12 @@ from fastapi.responses import RedirectResponse
 from pymongo.errors import ConnectionFailure
 from dotenv import load_dotenv
 import os
+from enum import Enum
+
+class JSendStatus(Enum):
+    SUCCESS = "success"
+    FAIL = "fail"
+    ERROR = "error"
 
 load_dotenv()
 MONGO_HOST = os.environ.get('MONGO_HOST', 'localhost')
@@ -32,9 +38,8 @@ db = client['leetcode_db']
 collection = db['problems']
 
 
-def jsend_response(status: str, data=None, message=None):
-    """Returns a JSend-style response object as a FastAPI server response"""
-    response = {"status": status}
+def jsend_response(status: JSendStatus, data=None, message=None):
+    response = {"status": status.value}
     if data is not None:
         response["data"] = data
     if message is not None:
@@ -48,9 +53,9 @@ async def root():
 @app.get("/questions")
 async def get_questions(
     difficulty: Optional[str] = None,
-    topic: Optional[str] = None,
-    language: Optional[str] = None
+    topic: Optional[str] = None
 ):
+    """Returns a list of questions (ids and titles) matching the given criteria: Difficulty and Topic"""
     query_filter = {
         "paid_only": False
     }
@@ -59,32 +64,44 @@ async def get_questions(
         query_filter["difficulty"] = difficulty
     if topic:
         query_filter["topic_tags"] = topic
-    if language:
-        query_filter["languages"] = language
 
-    cursor = collection.find(query_filter, {"_id": 0, "id": 1, "title": 1})
+    # Only return the id and title fields
+    cursor = collection.find(
+        query_filter, {"_id": False, "id": True, "title": True})
     questions = list(cursor)
 
-    return jsend_response("success", {"questions": questions})
+    if not questions:
+        raise HTTPException(
+            status_code=404, detail="No questions found with the given criteria.")
+
+    return jsend_response(JSendStatus.SUCCESS, {"questions": questions})
 
 @app.get("/questions/all")
 async def get_all_questions():
+    """Returns a list of all questions with all fields"""
     questions = list(collection.find({}))
+
+    if not questions:
+        raise HTTPException(
+            status_code=404, detail="No questions found in the database.")
+
     for question in questions:
         question["_id"] = str(question["_id"])
-    return jsend_response("success", {"questions": questions})
+    return jsend_response(JSendStatus.SUCCESS, {"questions": questions})
 
 @app.get("/question/{question_id}")
 async def get_one_question(question_id: str):
+    """Returns a single question matching the given id"""
     question = collection.find_one({"id": question_id})
-    if question:
-        question["_id"] = str(question["_id"])
-        return jsend_response("success", {"question": question})
-    else:
-        return jsend_response("fail", message="Question not found")
+
+    if not question:
+        raise HTTPException(status_code=404, detail="Question not found.")
+
+    question["_id"] = str(question["_id"])
+    return jsend_response(JSendStatus.SUCCESS, {"question": question})
 
 
 @app.exception_handler(HTTPException)
 async def custom_http_exception_handler(request: Request, exc: HTTPException):
     """Custom exception handler to return JSend-style responses for HTTPExceptions"""
-    return jsend_response("error", message=exc.detail)
+    return jsend_response(JSendStatus.ERROR, code=exc.status_code, message=exc.detail)
