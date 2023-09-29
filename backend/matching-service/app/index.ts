c5338cd2d97e477389ac9ee018aacebf33660664
Joel Wong <joelwongjy@gmail.com>
2023-09-29 15:27:45 +0800

    feat: link matching service to frontend

--- backend/matching-service/app/index.ts
+++ backend/matching-service/app/index.ts
@@ -1,44 +1,44 @@
-import express, { Application, Request, Response } from "express";
-import { createServer } from "http";
-import cors from "cors";
-import dotenv from "dotenv";
-import { Server } from "socket.io";
+import express, { Application, Request, Response } from 'express'
+import { createServer } from 'http'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import { Server } from 'socket.io'
 
-import { MATCHING_EVENTS } from "./src/matching/matching.constants";
-import { MatchingGateway } from "./src/matching/matching.gateway";
+import { MATCHING_EVENTS } from './src/matching/matching.constants'
+import { MatchingGateway } from './src/matching/matching.gateway'
 
 /**
  * Configuration
  */
-dotenv.config({ path: `.env.development` });
+dotenv.config({ path: `.env.development` })
 
-const app: Application = express();
-const httpServer = createServer(app);
+const app: Application = express()
+const httpServer = createServer(app)
 const io = new Server(httpServer, {
-  path: "/api/matching/websocket",
+  path: '/api/matching/websocket',
   cors: {
     origin: process.env.FRONTEND_ORIGIN,
-    credentials: true,
-  },
-});
+    credentials: true
+  }
+})
 
 /**
  * Middleware
  */
 app.use(
   cors({
-    origin: process.env.FRONTEND_ORIGIN,
+    origin: process.env.FRONTEND_ORIGIN
   })
-);
+)
 
 /**
  * Routes
  */
-app.get("/", (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send(
-    "Welcome to PeerPrep (Matching service) - A project for NUS CS3219 - Group 3"
-  );
-});
+    'Welcome to PeerPrep (Matching service) - A project for NUS CS3219 - Group 3'
+  )
+})
 
 /**
  * Start the server
@@ -46,20 +46,20 @@ app.get("/", (req: Request, res: Response) => {
 httpServer.listen(process.env.PORT, () => {
   console.log(`Matching Service is live and running ⚡
 Container URL: http://localhost:${process.env.PORT}
-Local development URL: http://localhost:8004`);
-});
+Local development URL: http://localhost:8004`)
+})
 
-const matchingGateway = new MatchingGateway();
+const matchingGateway = new MatchingGateway()
 
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
   socket.on(MATCHING_EVENTS.JOIN_ROOM, (user, preferences) => {
-    const matched = matchingGateway.joinRandomRoom({ user, preferences });
+    const matched = matchingGateway.joinRandomRoom({ user, preferences })
     if (matched) {
-      socket.join(matched.roomId);
-      io.to(matched.roomId).emit(MATCHING_EVENTS.FOUND_ROOM, matched);
+      socket.join(matched.roomId)
+      io.to(matched.roomId).emit(MATCHING_EVENTS.FOUND_ROOM, matched)
     } else {
-      const newRoom = matchingGateway.createRoom({ user, preferences });
-      socket.join(newRoom);
+      const newRoom = matchingGateway.createRoom({ user, preferences })
+      socket.join(newRoom)
     }
-  });
-});
+  })
+})
