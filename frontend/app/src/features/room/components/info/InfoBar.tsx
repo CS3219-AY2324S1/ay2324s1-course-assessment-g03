d56565e80434525383daec27c483ead6fc1a5fbe
raynerljm <raynerljm@gmail.com>
2023-10-03 15:51:29 +0800

    refactor: refactor user card

--- frontend/app/src/features/room/components/info/InfoBar.tsx
+++ frontend/app/src/features/room/components/info/InfoBar.tsx
@@ -1,24 +1,24 @@
-import { HStack } from "@chakra-ui/react"
-import { DifficultyBadge, TopicBadge } from "@/features/room"
-import { DifficultyType } from "@/constants/question"
-import { UserCard } from "@/features/matching"
+import { HStack } from "@chakra-ui/react";
+import { DifficultyBadge, TopicBadge } from "@/features/room";
+import { DifficultyType } from "@/constants/question";
+import { UserInfo } from "@/components/UserInfo/UserInfo";
 
 interface InfoBarProps {
-    difficulty: DifficultyType;
-    topic: string;
+  difficulty: DifficultyType;
+  topic: string;
 }
 
 export const InfoBar = ({ difficulty, topic }: InfoBarProps) => {
-    return (
-        <HStack justifyContent={"space-between"}>
-            <HStack>
-                <UserCard user={undefined} />
-                <UserCard user={undefined} />
-            </HStack>
-            <HStack>
-                <DifficultyBadge difficulty={difficulty} />
-                <TopicBadge topic={topic} />
-            </HStack>
-        </HStack>
-    )
-}
+  return (
+    <HStack justifyContent={"space-between"}>
+      <HStack>
+        <UserInfo user={undefined} />
+        <UserInfo user={undefined} />
+      </HStack>
+      <HStack>
+        <DifficultyBadge difficulty={difficulty} />
+        <TopicBadge topic={topic} />
+      </HStack>
+    </HStack>
+  );
+};
