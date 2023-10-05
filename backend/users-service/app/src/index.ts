a0c6ef84231d8294a29f618814a259b7970f80b7
Shem Maleriado <shem.maleriado@gmail.com>
2023-10-06 03:01:17 +0800

    feat: add endpoints

--- backend/users-service/app/src/index.ts
+++ backend/users-service/app/src/index.ts
@@ -1,7 +1,57 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from 'elysia'
+import { swagger } from '@elysiajs/swagger'
+import { cors } from '@elysiajs/cors'
+import { helmet } from 'elysia-helmet'
+import jwt from '@elysiajs/jwt'
+import { PrismaClient } from '@prisma/client'
+import { logger } from '@bogeychan/elysia-logger'
 
-const app = new Elysia().get("/", () => "Users service").listen(80);
+const db = new PrismaClient()
+
+const app = new Elysia({ prefix: '/users' })
+  .use(cors())
+  .use(helmet())
+  .use(
+    jwt({
+      name: 'jwt',
+      secret: process.env.JWT_SECRET as string
+    })
+  )
+  .use(logger())
+  .use(swagger())
+  .model({
+    user: t.Object({
+      id: t.String(),
+      email: t.String()
+    })
+  })
+  .get(
+    '/:id',
+    async ({ params: { id } }) =>
+      await db.user.findUnique({
+        where: {
+          id
+        }
+      })
+  )
+  .post('/', async ({ body }) => await db.user.create({ data: body }), {
+    body: 'user'
+  })
+  .put(
+    '/',
+    async ({ body: { id, ...data } }) =>
+      await db.user.update({
+        where: { id },
+        data
+      }),
+    { body: 'user' }
+  )
+  .delete(
+    '/:id',
+    async ({ params: { id } }) => await db.user.delete({ where: { id } })
+  )
+  .listen(process.env.PORT as string)
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+  `Users service is running at ${app.server?.hostname}:${app.server?.port}`
+)
