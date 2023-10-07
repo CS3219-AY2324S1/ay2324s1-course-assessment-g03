c915511337dbdf2d5b910f3658ef3604520a1702
Shem Maleriado <shem.maleriado@gmail.com>
2023-10-07 15:25:38 +0800

    feat: add avatarUrl and role to user schema, reduce question schema

--- backend/users-service/app/src/index.ts
+++ backend/users-service/app/src/index.ts
@@ -5,6 +5,7 @@ import { helmet } from 'elysia-helmet'
 import jwt from '@elysiajs/jwt'
 import { PrismaClient } from '@prisma/client'
 import { logger } from '@bogeychan/elysia-logger'
+import { Role } from './constants'
 
 const db = new PrismaClient()
 
@@ -19,32 +20,45 @@ const app = new Elysia({ prefix: '/users' })
   )
   .use(logger())
   .use(swagger())
-  .model({
-    user: t.Object({
-      id: t.String(),
-      email: t.String()
-    })
-  })
   .get(
     '/:id',
     async ({ params: { id } }) =>
       await db.user.findUnique({
         where: {
           id
+        },
+        include: {
+          questions: true
         }
       })
   )
-  .post('/', async ({ body }) => await db.user.create({ data: body }), {
-    body: 'user'
-  })
+  .post(
+    '/',
+    async ({ body: { avatarUrl, email } }) =>
+      await db.user.create({
+        data: {
+          avatarUrl,
+          email,
+          role: Role.User
+        }
+      }),
+    {
+      body: t.Object({ avatarUrl: t.String(), email: t.String() })
+    }
+  )
   .put(
     '/',
-    async ({ body: { id, ...data } }) =>
-      await db.user.update({
-        where: { id },
-        data
+    async ({ body: { avatarUrl, email } }) =>
+      await db.user.create({
+        data: {
+          avatarUrl,
+          email,
+          role: 'user'
+        }
       }),
-    { body: 'user' }
+    {
+      body: t.Object({ avatarUrl: t.String(), email: t.String() })
+    }
   )
   .delete(
     '/:id',
