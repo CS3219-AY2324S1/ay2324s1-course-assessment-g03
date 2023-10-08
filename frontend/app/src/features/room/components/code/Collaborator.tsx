14f2ece222acd7669cc06fd51391d5e656eaa229
zechajw <zecharyajw@gmail.com>
2023-10-08 22:29:01 +0800

    feat: style dropdown

--- frontend/app/src/features/room/components/code/Collaborator.tsx
+++ frontend/app/src/features/room/components/code/Collaborator.tsx
@@ -8,113 +8,96 @@ import { SingleValue } from "chakra-react-select";
 import { LanguageSupport } from "node_modules/@codemirror/language/dist";
 
 interface CollaboratorProps {
-    roomId: string;
+  roomId: string;
 }
 
 export const Collaborator = ({ roomId }: CollaboratorProps) => {
-    const [renderQuestion, setRenderQuestion] = useState(true);
-    const [socket, setSocket] = useState<Socket | null>(null);
-    const [currentLanguage, setCurrentLanguage] = useState(LANGUAGES.Python)
+  const [renderQuestion, setRenderQuestion] = useState(true);
+  const [socket, setSocket] = useState<Socket | null>(null);
+  const [currentLanguage, setCurrentLanguage] = useState(LANGUAGES.Python)
 
-    useEffect(() => {
-        const connectSocket = io("http://localhost:8005", {
-            query: {
-                roomId: roomId
-            }
-        });
-        setSocket(connectSocket);
-        return () => {
-            if (connectSocket) {
-                connectSocket.disconnect();
-                setSocket(null);
-            }
-        };
-    }, [roomId]);
+  useEffect(() => {
+    const connectSocket = io("http://localhost:8005", {
+      query: {
+        roomId: roomId
+      }
+    });
+    setSocket(connectSocket);
+    return () => {
+      if (connectSocket) {
+        connectSocket.disconnect();
+        setSocket(null);
+      }
+    };
+  }, [roomId]);
 
-    const handleLanguageChange = (e: SingleValue<{
-        label: string;
-        value: LanguageSupport;
-    }>) => {
-        setCurrentLanguage(e?.value ?? LANGUAGES.Python)
-    }
+  const handleLanguageChange = (e: SingleValue<{
+    label: string;
+    value: LanguageSupport;
+  }>) => {
+    setCurrentLanguage(e?.value ?? LANGUAGES.Python)
+  }
 
-    //TODO: update options when in
-    const options = (
-        <HStack>
-            <Dropdown
-                size="sm"
-                title="Question"
-                placeholder="Select question"
-                options={[]}
-            />
-            <Dropdown
-                size="sm"
-                title="Language"
-                placeholder="Select language"
-                options={Object.entries(LANGUAGES).map(([languageName, languageSupport]) => ({ label: languageName, value: languageSupport }))}
-                onChangeHandler={handleLanguageChange}
-            />
-        </HStack>
-    );
+  //TODO: update options when in
+  const options = (
+    <HStack>
+      <Dropdown
+        size="sm"
+        title="Question"
+        placeholder="Select question"
+        options={[]}
+      />
+      <Dropdown
+        size="sm"
+        title="Language"
+        placeholder="Select language"
+        options={Object.entries(LANGUAGES).map(([languageName, languageSupport]) => ({ label: languageName, value: languageSupport }))}
+        onChangeHandler={handleLanguageChange}
+      />
+    </HStack>
+  );
 
-    const codeEditor = socket ? (
-        <CodeEditor socket={socket} roomId={roomId} language={currentLanguage} />
-    ) : (
-        <Box>Loading</Box>
-    );
+  const codeEditor = socket ? (
+    <CodeEditor socket={socket} roomId={roomId} language={currentLanguage} />
+  ) : (
+    <Box>Loading</Box>
+  );
 
-    const visibleView = (
-        <HStack height="full" width="full">
-            <VStack align="left" height="full" width="50%">
-                {options}
-                <QuestionDetails details={"Question Details"} />
-            </VStack>
-            <VStack align="left" height="full" width="50%">
-                <Text textStyle="sm">Code</Text>
-                {codeEditor}
-            </VStack>
-        </HStack>
-<<<<<<< HEAD
-    );
-=======
-    )
->>>>>>> db2103e (Add collab-service frontend)
+  const visibleView = (
+    <HStack height="full" width="full">
+      <VStack align="left" height="full" width="50%">
+        {options}
+        <QuestionDetails details={"Question Details"} />
+      </VStack>
+      <VStack align="left" height="full" width="50%">
+        <Text textStyle="sm">Code</Text>
+        {codeEditor}
+      </VStack>
+    </HStack>
+  );
 
-    const hiddenView = (
-        <VStack align="left" height="full" width="full">
-            {options}
-            {codeEditor}
-        </VStack>
-<<<<<<< HEAD
-    );
-=======
-    )
->>>>>>> db2103e (Add collab-service frontend)
+  const hiddenView = (
+    <VStack align="left" height="full" width="full">
+      {options}
+      {codeEditor}
+    </VStack>
+  );
 
-    return (
-        <VStack align="left" height="full" width="full">
-            <Box height="full" width="full">
-                {renderQuestion ? visibleView : hiddenView}
-            </Box>
-            <Box>
-<<<<<<< HEAD
-    <Button
-        size="xs"
-        background="#3F3F46"
-        textColor="white"
-        onClick={() => setRenderQuestion(!renderQuestion)}
-    >
-        {renderQuestion ? "Hide" : "Show"} Question
-    </Button>
-            </Box >
-        </VStack >
-    );
+  return (
+    <VStack align="left" height="full" width="full">
+      <Box height="full" width="full">
+        {renderQuestion ? visibleView : hiddenView}
+      </Box>
+      <Box>
+        <Button
+          size="xs"
+          background="#3F3F46"
+          textColor="white"
+          onClick={() => setRenderQuestion(!renderQuestion)}
+        >
+          {renderQuestion ? "Hide" : "Show"} Question
+        </Button>
+      </Box >
+    </VStack >
+  );
 };
-=======
-                <Button size="xs" background="#3F3F46" textColor="white" onClick={() => setRenderQuestion(!renderQuestion)}>{renderQuestion ? "Hide" : "Show"} Question</Button>
-            </Box>
-        </VStack>
-    )
-
-}
->>>>>>> db2103e (Add collab-service frontend)
