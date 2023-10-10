85385a3e713193a3f528369b85496ea88edc2ff9
Shem Maleriado <shem.maleriado@gmail.com>
2023-10-10 23:49:42 +0800

    feat: add role validation TODO comment

--- backend/users-service/app/src/controllers/users.controller.ts
+++ backend/users-service/app/src/controllers/users.controller.ts
@@ -4,7 +4,7 @@ import { create, read, remove, update } from "../services/users.service";
 export const del = ({ params: { id } }: { params: { id: string } }) => remove(id)
 
 export const get = ({ params: { id } }: { params: { id: string } }) => read(id)
-
+// TODO: Implement role validation
 export const post = ({ body }: { body: CreateUserBody }) => create(body)
 
 export const put = ({ body, params: { id } }: { body: UpdateUserBody, params: { id: string } }) => update(id, body)
