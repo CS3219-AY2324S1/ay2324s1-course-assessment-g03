8cacb382963d00c6669a7e68b53b609e6f4e0a39
Joseph Aedan Marcus <josephaedan@gmail.com>
2023-10-10 16:34:20 +0800

    Add nullable to Github user name property

--- frontend/app/src/types/user.ts
+++ frontend/app/src/types/user.ts
@@ -3,9 +3,9 @@ import { z } from "zod";
 export const userSchema = z.object({
   // TODO: Remove optional once settled
   id: z.string().optional(),
-  name: z.string().optional(),
+  name: z.string().optional().nullable(),
   email: z.string().email(),
-  avatarUrl: z.string().url().optional(),
+  avatarUrl: z.string().url().optional().nullable(),
 });
 
 export type User = z.infer<typeof userSchema>;
