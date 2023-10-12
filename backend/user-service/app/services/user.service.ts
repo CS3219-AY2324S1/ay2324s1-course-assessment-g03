8a32257c35dae49a0eaee222a9c728f466f8cf16
sheimoria <shem.maleriado@gmail.com>
2023-10-12 16:26:25 +0800

    feat: add error handling

--- backend/user-service/app/services/user.service.ts
+++ backend/user-service/app/services/user.service.ts
@@ -2,11 +2,42 @@ import { db } from "./db.service";
 import { UserPersonalData } from "../types/userPersonalData";
 
 export const userService = {
-  create: (body: UserPersonalData) => db.user.create({ data: body }),
-  delete: (id: string) => db.user.delete({ where: { id } }),
-  findById: (id: string) =>
-    db.user.findUnique({ where: { id }, include: { questionAttempts: true } }),
-  findByEmail: (email: string) => db.user.findUnique({ where: { email } }),
-  update: (id: string, body: UserPersonalData) =>
-    db.user.update({ where: { id }, data: body }),
+  create: async (body: UserPersonalData) => {
+    try {
+      return await db.user.create({ data: body });
+    } catch (e) {
+      throw e;
+    }
+  },
+  delete: async (id: string) => {
+    try {
+      return await db.user.delete({ where: { id } });
+    } catch (e) {
+      throw e;
+    }
+  },
+  findById: async (id: string) => {
+    try {
+      return await db.user.findUnique({
+        where: { id },
+        include: { questionAttempts: true },
+      });
+    } catch (e) {
+      throw e;
+    }
+  },
+  findByEmail: async (email: string) => {
+    try {
+      return await db.user.findUnique({ where: { email } });
+    } catch (e) {
+      throw e;
+    }
+  },
+  update: async (id: string, body: UserPersonalData) => {
+    try {
+      return await db.user.update({ where: { id }, data: body });
+    } catch (e) {
+      throw e;
+    }
+  },
 };
