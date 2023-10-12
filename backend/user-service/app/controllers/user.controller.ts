cc882a077557144e0a27a93f04517112796a756a
sheimoria <shem.maleriado@gmail.com>
2023-10-12 16:26:25 +0800

    feat: add error handling

--- backend/user-service/app/controllers/user.controller.ts
+++ backend/user-service/app/controllers/user.controller.ts
@@ -1,26 +1,43 @@
-import { successApiResponse } from "../libs";
+import { failApiResponse, successApiResponse } from "../libs";
 import { userService } from "../services/user.service";
 import { Request, Response } from "express";
 
 export const userController = {
   getById: async (req: Request, res: Response) => {
-    res.send(successApiResponse(await userService.findById(req.params.userId)));
+    try {
+      res.send(successApiResponse(userService.findById(req.params.userId)));
+    } catch (e) {
+      res.send(failApiResponse(e));
+    }
   },
   getByEmail: async (req: Request, res: Response) => {
-    res.send(
-      successApiResponse(await userService.findByEmail(req.params.email))
-    );
+    try {
+      res.send(successApiResponse(userService.findByEmail(req.params.email)));
+    } catch (e) {
+      res.send(failApiResponse(e));
+    }
   },
   post: async (req: Request, res: Response) => {
-    console.log(req.body);
-    res.send(successApiResponse(await userService.create(req.body)));
+    try {
+      res.send(successApiResponse(userService.create(req.body)));
+    } catch (e) {
+      res.send(failApiResponse(e));
+    }
   },
   put: async (req: Request, res: Response) => {
-    res.send(
-      successApiResponse(await userService.update(req.params.userId, req.body))
-    );
+    try {
+      res.send(
+        successApiResponse(userService.update(req.params.userId, req.body))
+      );
+    } catch (e) {
+      res.send(failApiResponse(e));
+    }
   },
   delete: async (req: Request, res: Response) => {
-    res.send(successApiResponse(await userService.delete(req.params.userId)));
+    try {
+      res.send(successApiResponse(userService.delete(req.params.userId)));
+    } catch (e) {
+      res.send(failApiResponse(e));
+    }
   },
 };
