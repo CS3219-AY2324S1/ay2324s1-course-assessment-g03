bf37f194c35f700402ee951897cca84541196437
sheimoria <shem.maleriado@gmail.com>
2023-10-13 02:52:52 +0800

    fix: CRUD endpoints

--- backend/user-service/app/controllers/user.controller.ts
+++ backend/user-service/app/controllers/user.controller.ts
@@ -2,42 +2,72 @@ import { failApiResponse, successApiResponse } from "../libs";
 import { userService } from "../services/user.service";
 import { Request, Response } from "express";
 
+const ERROR_MESSAGE = "An error occurred";
+
 export const userController = {
   getById: async (req: Request, res: Response) => {
     try {
-      res.send(successApiResponse(userService.findById(req.params.userId)));
+      const user = await userService.findById(req.params.userId);
+      res.send(successApiResponse(user));
     } catch (e) {
-      res.send(failApiResponse(e));
+      let message = ERROR_MESSAGE;
+      if (e instanceof Error) {
+        console.log(e.message);
+        message = e.message;
+      }
+      res.send(failApiResponse({ message }));
     }
   },
   getByEmail: async (req: Request, res: Response) => {
     try {
-      res.send(successApiResponse(userService.findByEmail(req.params.email)));
+      const user = await userService.findByEmail(req.params.email);
+      res.send(successApiResponse(user));
     } catch (e) {
-      res.send(failApiResponse(e));
+      let message = ERROR_MESSAGE;
+      if (e instanceof Error) {
+        console.log(e.message);
+        message = e.message;
+      }
+      res.send(failApiResponse({ message }));
     }
   },
   post: async (req: Request, res: Response) => {
     try {
-      res.send(successApiResponse(userService.create(req.body)));
+      const user = await userService.create(req.body);
+      res.send(successApiResponse(user));
     } catch (e) {
-      res.send(failApiResponse(e));
+      let message = ERROR_MESSAGE;
+      if (e instanceof Error) {
+        console.log(e.message);
+        message = e.message;
+      }
+      res.send(failApiResponse({ message }));
     }
   },
   put: async (req: Request, res: Response) => {
     try {
-      res.send(
-        successApiResponse(userService.update(req.params.userId, req.body))
-      );
+      const user = await userService.update(req.params.userId, req.body);
+      res.send(successApiResponse(user));
     } catch (e) {
-      res.send(failApiResponse(e));
+      let message = ERROR_MESSAGE;
+      if (e instanceof Error) {
+        console.log(e.message);
+        message = e.message;
+      }
+      res.send(failApiResponse({ message }));
     }
   },
   delete: async (req: Request, res: Response) => {
     try {
-      res.send(successApiResponse(userService.delete(req.params.userId)));
+      const user = await userService.delete(req.params.userId);
+      res.send(successApiResponse(user));
     } catch (e) {
-      res.send(failApiResponse(e));
+      let message = ERROR_MESSAGE;
+      if (e instanceof Error) {
+        console.log(e.message);
+        message = e.message;
+      }
+      res.send(failApiResponse({ message }));
     }
   },
 };
