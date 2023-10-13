89000e87c83623c3411f867e01b9b9283dddb857
Joel Wong <joelwongjy@gmail.com>
2023-10-13 10:53:04 +0800

    feat: create timer for matching process

--- frontend/app/src/features/matching/components/FindingMatchCard.tsx
+++ frontend/app/src/features/matching/components/FindingMatchCard.tsx
@@ -1,36 +1,93 @@
-import { Card } from "@/components";
-import { VStack, Text, HStack } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { Button, VStack, Text, HStack } from "@chakra-ui/react";
 import { UserCard } from ".";
+import { Card } from "@/components";
 import { useAuth } from "@/hooks";
-import { CustomButton } from "@/components/Layout/CustomButton";
+import { User } from "@/types/user";
+import { useStopwatch } from "react-timer-hook";
 
 type Props = {
+  otherUser: User | undefined;
   leaveCallback: () => void;
 };
 
-export const FindingMatchCard = ({ leaveCallback }: Props) => {
+const TIMEOUT_DURATION = 3;
+
+export const FindingMatchCard = ({ otherUser, leaveCallback }: Props) => {
   const { data } = useAuth();
+  const [expired, setExpired] = useState(false);
+
+  const { seconds, minutes } = useStopwatch({ autoStart: true });
+  const [timeToJoin, setTimeToJoin] = useState(5);
 
   const user = data?.user;
 
+  useEffect(() => {
+    const timer1 = setTimeout(() => {
+      setExpired(true);
+      setTimeout(() => {
+        leaveCallback();
+      }, 3000);
+    }, TIMEOUT_DURATION * 1000);
+
+    return () => {
+      clearTimeout(timer1);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!otherUser || timeToJoin <= 0) return;
+
+    const timerId = setTimeout(() => {
+      setTimeToJoin(timeToJoin - 1);
+    }, 1000);
+
+    return () => clearTimeout(timerId);
+  }, [otherUser, timeToJoin]);
+
+  const headerText = (): string => {
+    if (otherUser) {
+      return "Match found, joining in";
+    }
+    if (expired) {
+      return "No match found, try again later!";
+    }
+    return "Finding a match";
+  };
+
   return (
-    <Card
-      backgroundColor="light.600"
-      borderRadius="1.2rem"
-      padding="2rem"
-      w="full"
-      maxW="36rem"
-    >
+    <Card w="full" maxW="36rem">
       <VStack gap="1.25rem">
-        <Text textStyle="heading-xs" alignSelf="start">
-          Finding a match...
-        </Text>
+        <HStack w="full" justify="space-between">
+          <Text textStyle="heading-md">{headerText()}</Text>
+          <Text textStyle="heading-md">
+            {otherUser ? (
+              <span>{timeToJoin}</span>
+            ) : (
+              <>
+                <span>{minutes}:</span>
+                <span>{seconds}</span>
+              </>
+            )}
+          </Text>
+        </HStack>
         <HStack gap="1.25rem" w="full">
           <UserCard user={user} />
-          <UserCard user={undefined} />
+          <UserCard user={otherUser} />
         </HStack>
         <HStack alignSelf="end">
-          <CustomButton onClick={leaveCallback}>Leave room</CustomButton>
+          <Button
+            colorScheme="light"
+            border="1px solid"
+            borderRadius="6.25rem"
+            borderColor="light.400"
+            _hover={{
+              borderColor: "light.300",
+            }}
+            onClick={leaveCallback}
+          >
+            Leave room
+          </Button>
         </HStack>
       </VStack>
     </Card>
