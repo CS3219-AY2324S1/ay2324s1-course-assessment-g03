4b9d10febb0e5fad9ff44a1f6d829028f000362f
sheimoria <shem.maleriado@gmail.com>
2023-10-17 04:25:57 +0800

    refactor: folder structure

--- frontend/app/src/features/admin/questions/api/useQuestions.tsx
+++ frontend/app/src/features/admin/questions/api/useQuestions.tsx
@@ -7,9 +7,22 @@ import { questionSchema } from "@/types/question";
 import { z } from "zod";
 import { safeParse } from "@/lib/safeParse";
 
+type UseQuestionsOptions = {
+  pageNum: number;
+  pageSize: number;
+};
+
 const getQuestionsResponseSchema = makeSuccessResponseSchema(
   z.object({
     questions: z.array(questionSchema),
+    pagination: z.object({
+      current_page: z.number(),
+      limit: z.number(),
+      order: z.string(),
+      sort_by: z.string(),
+      total_pages: z.number(),
+      total_questions: z.number(),
+    }),
   }),
 );
 
@@ -17,11 +30,16 @@ export type GetQuestionsResponse = z.infer<typeof getQuestionsResponseSchema>;
 
 const queryKey = [QUESTIONS_QUERY_KEY];
 
-export const useQuestions = () => {
+export const useQuestions = ({ pageNum, pageSize }: UseQuestionsOptions) => {
   return useQuery({
     queryKey,
     queryFn: async () => {
-      const { data } = await backendApi.get(`${API_ENDPOINT.ADMIN_QUESTIONS}`);
+      const { data } = await backendApi.get(`${API_ENDPOINT.ADMIN_QUESTIONS}`, {
+        params: {
+          page: pageNum,
+          limit: pageSize,
+        },
+      });
       return safeParse(getQuestionsResponseSchema, data);
     },
   });
