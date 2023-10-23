f30df38b5e5b9d124d0b7dffcdfae49eadc8824e
zechajw <zecharyajw@gmail.com>
2023-10-23 15:25:38 +0800

    Change question details component to use react-query

--- frontend/app/src/features/room/components/code/QuestionDetails.tsx
+++ frontend/app/src/features/room/components/code/QuestionDetails.tsx
@@ -1,41 +1,35 @@
-import { API_ENDPOINT, API_RESPONSE_STATUS } from "@/constants/api";
-import { makeSuccessResponseSchema } from "@/lib/api";
-import { backendApi } from "@/lib/axios";
 import { Box, Text } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import { z } from "zod";
+import { useGetQuestionDetails } from "../../api/useGetQuestionDetails";
 
 interface QuestionDetailsProps {
     questionId: number
 }
 
-const getQuestionDetailsResponseSchema = makeSuccessResponseSchema(z.object({
-    question: z.object({
-        description: z.string()
-    })
-}))
-
 export const QuestionDetails = ({ questionId }: QuestionDetailsProps) => {
 
-    const [details, setQuestionDetails] = useState<string>()
-    const [loading, isLoading] = useState(true)
+    const { isLoading, isError, data } = useGetQuestionDetails(questionId)
 
-    useEffect(() => {
-        async function getQuestionDetails() {
-            const { data } = await backendApi.get(`${API_ENDPOINT.QUESTIONS}/${questionId}`)
-            const questionData = getQuestionDetailsResponseSchema.parse(data)
-            if (questionData.status === API_RESPONSE_STATUS.SUCCESS) {
-                setQuestionDetails(questionData.data.question.description)
-                isLoading(false)
-            }
-        }
+    if (isLoading) {
+        return (
+            <Box flex={1} padding={"0.75rem"} border="1px" borderColor={"ActiveBorder"} rounded="md" height="full">
+                <Text whiteSpace={"pre-wrap"}>{"Loading..."}</Text>
+            </Box>
+        )
+    }
 
-        getQuestionDetails()
-    }, [questionId])
+    if (isError || !data) {
+        return (
+            <Box flex={1} padding={"0.75rem"} border="1px" borderColor={"ActiveBorder"} rounded="md" height="full">
+                <Text whiteSpace={"pre-wrap"}>{"Choose a Question"}</Text>
+            </Box>
+        )
+    }
+
+    const { description } = data.data.question
 
     return (
         <Box flex={1} padding={"0.75rem"} border="1px" borderColor={"ActiveBorder"} rounded="md" height="full">
-            <Text whiteSpace={"pre-wrap"}>{loading ? "Loading..." : details}</Text>
+            <Text whiteSpace={"pre-wrap"}>{description}</Text>
         </Box>
     )
 }
