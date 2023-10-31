47b5a1fd5d9f618a0f90f8182ffc307bf0ea4c7b
sheimoria <shem.maleriado@gmail.com>
2023-10-31 15:43:43 +0800

    fix: update react-query

--- frontend/app/src/features/room/api/useGetQuestionDetails.ts
+++ frontend/app/src/features/room/api/useGetQuestionDetails.ts
@@ -1,27 +1,31 @@
-import { API_ENDPOINT } from "@/constants/api"
-import { makeSuccessResponseSchema } from "@/lib/api"
-import { backendApi } from "@/lib/axios"
-import { useQuery } from "react-query"
-import z from "zod"
+import { API_ENDPOINT } from "@/constants/api";
+import { makeSuccessResponseSchema } from "@/lib/api";
+import { backendApi } from "@/lib/axios";
+import { useQuery } from "@tanstack/react-query";
+import z from "zod";
 
-const GET_QUESTION_DETAILS_KEY = "get-question-details"
+const GET_QUESTION_DETAILS_KEY = "get-question-details";
 
-const getQuestionDetailsResponseSchema = makeSuccessResponseSchema(z.object({
+const getQuestionDetailsResponseSchema = makeSuccessResponseSchema(
+  z.object({
     question: z.object({
-        description: z.string()
-    })
-}))
+      description: z.string(),
+    }),
+  }),
+);
 
 const getQuestionDetails = async (questionId: number) => {
-    const { data } = await backendApi.get(`${API_ENDPOINT.QUESTIONS}/${questionId}`)
-    return getQuestionDetailsResponseSchema.parse(data)
-}
+  const { data } = await backendApi.get(
+    `${API_ENDPOINT.QUESTIONS}/${questionId}`,
+  );
+  return getQuestionDetailsResponseSchema.parse(data);
+};
 
 export const useGetQuestionDetails = (questionId: number | undefined) => {
-    return useQuery({
-        queryKey: [GET_QUESTION_DETAILS_KEY],
-        queryFn: questionId ? () => getQuestionDetails(questionId) : undefined,
-        enabled: questionId !== undefined,
-        retry: false
-    })
-}
+  return useQuery({
+    queryKey: [GET_QUESTION_DETAILS_KEY],
+    queryFn: questionId ? () => getQuestionDetails(questionId) : undefined,
+    enabled: questionId !== undefined,
+    retry: false,
+  });
+};
