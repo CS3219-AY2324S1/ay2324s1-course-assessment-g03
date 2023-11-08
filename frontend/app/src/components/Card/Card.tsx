119fce1d5f26c2dd7c16be1e3c5196489fda9e81
Joel Wong <joelwongjy@gmail.com>
2023-11-09 01:58:16 +0800

    fix: navbar shown when not logged in

--- frontend/app/src/components/Card/Card.tsx
+++ frontend/app/src/components/Card/Card.tsx
@@ -13,7 +13,6 @@ export const Card = ({ children, ...boxProps }: CardProps) => {
       borderColor="rgba(255,255,255,0.08)"
       borderWidth="1px"
       boxShadow="0 4px 12px rgba(0,0,0,.24)"
-      background="dark.950"
       p="2rem"
       {...boxProps}
     >
